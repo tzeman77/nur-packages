2133d54c08602a4e08ca0d3f85023d9b34e88b4f
Tomas Zeman <tomas@functionals.cz>
2020-05-31 14:38:03 +0200

    firehol: imported from nixpkgs-patches

--- modules/default.nix
+++ modules/default.nix
@@ -2,6 +2,7 @@
   # Add your NixOS modules here
   #
   # my-module = ./my-module;
+  firehol = ./firehol.nix;
   runit = ./runit.nix;
   zabbix-runit = ./zabbix-runit.nix;
 }
---
+++ modules/firehol.nix
@@ -0,0 +1,73 @@
+{ config, pkgs, lib, ... }:
+
+with lib;
+
+let
+
+  cfg = config.networking.firehol;
+  pkg = pkgs.firehol;
+
+in
+
+{
+
+  ###### interface
+
+  options = {
+
+    networking.firehol.enable = mkOption {
+      default = false;
+      description = "Whether to enable firehol firewall.";
+    };
+
+    networking.firehol.configuration = mkOption {
+      default = ''
+        version 5
+
+        interface any world
+          client all accept
+      '';
+      description = "Firehol configuration, see firehol.conf(5).";
+    };
+
+  };
+
+  ###### implementation
+
+  config = mkIf cfg.enable {
+
+    networking.firewall.enable = false; # mutually exclusive with nixos default firewall.
+
+    boot.kernelModules = [ "configs" ];
+
+    environment.etc."firehol/firehol.conf".text = cfg.configuration;
+
+    environment.systemPackages = [ pkg pkgs.iptables ];
+
+    systemd.services.firehol = {
+      description = "Firehol firewall";
+      wantedBy = [ "network.target" ];
+      after = [ "network-interfaces.target" ];
+      path = with pkgs; [ gnugrep findutils gawk gnused procps gzip iproute iprange
+        which iptables kmod  ];
+
+      preStart = "mkdir -p /var/spool";
+
+      serviceConfig = {
+        Type = "oneshot";
+        RemainAfterExit = true;
+        ExecStop = ''
+          ${pkg}/sbin/firehol stop
+        '';
+      };
+
+      script = ''
+        ${pkg}/sbin/firehol start
+      '';
+    };
+
+  };
+
+}
+
+# vim: et ts=2 sw=2 
